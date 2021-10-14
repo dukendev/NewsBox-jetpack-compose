abb012d74537f13c76852592afc528a5b745f08f
Sanjeet Yadav <ysanjeet535@gmail.com>
2021-10-14 16:53:55 +0530

    added search bar on explore screen

--- app/src/main/java/com/ysanjeet535/newsbox/ui/view/explore/ExploreScreen.kt
+++ app/src/main/java/com/ysanjeet535/newsbox/ui/view/explore/ExploreScreen.kt
@@ -3,13 +3,43 @@ package com.ysanjeet535.newsbox.ui.view.explore
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ExploreScreenContent(){
-    Box(modifier = Modifier.fillMaxSize().background(Color.Blue)) {
+    Box(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize()
+        .background(Color.Transparent)) {
 
+        SearchBar(modifier = Modifier.fillMaxWidth()){
+            //lambda to perform search options
+        }
     }
+}
+
+@Preview
+@Composable
+fun SearchBar(modifier: Modifier = Modifier,onSearch: (String)->Unit={}) {
+
+    var text by remember { mutableStateOf("")}
+    OutlinedTextField(
+        value = text,
+        onValueChange = {
+            text = it
+            onSearch(it)
+                        },
+        modifier = modifier,
+        label = { Text(text = "Search")},
+        maxLines = 1
+    )
+
 }
--- app/src/main/java/com/ysanjeet535/newsbox/ui/view/profile/ProfileScreen.kt
+++ app/src/main/java/com/ysanjeet535/newsbox/ui/view/profile/ProfileScreen.kt
@@ -2,14 +2,20 @@ package com.ysanjeet535.newsbox.ui.view.profile
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ProfileScreenContent(){
-    Box(modifier = Modifier.fillMaxSize().background(Color.Magenta)) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Magenta)) {
 
     }
 }
+
