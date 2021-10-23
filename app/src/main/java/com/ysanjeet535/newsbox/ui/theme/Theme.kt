e5eb529096d9ac6252a39a931e75ec1fa951f5d0
Sanjeet Yadav <ysanjeet535@gmail.com>
2021-10-23 20:45:12 +0530

    changed status bar color

--- app/src/main/java/com/ysanjeet535/newsbox/ui/theme/Theme.kt
+++ app/src/main/java/com/ysanjeet535/newsbox/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -36,6 +37,10 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun NewsBoxTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(RedBoxMedium)
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
