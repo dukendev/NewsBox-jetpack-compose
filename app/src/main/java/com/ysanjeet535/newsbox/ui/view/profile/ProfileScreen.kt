d5735a47a3d1dba049a5122099bfc225a49983cf
Sanjeet Yadav <ysanjeet535@gmail.com>
2021-10-23 20:59:38 +0530

    changed profile screen header

--- app/src/main/java/com/ysanjeet535/newsbox/ui/view/profile/ProfileScreen.kt
+++ app/src/main/java/com/ysanjeet535/newsbox/ui/view/profile/ProfileScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -131,19 +132,15 @@ fun ProfileHeader(int: Int=0){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = rememberImagePainter(data = "https://wallpaperaccess.com/full/547408.jpg"),
+            painter = painterResource(id = R.drawable.ic_saved_news),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(200.dp)
-                .clip(CircleShape)
-                .border(2.dp, RedBoxDark, CircleShape)
-
         )
         Spacer(modifier = Modifier.height(32.dp))
         Row(modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.SpaceBetween) {
-           Text(text = "Barney",style = MaterialTheme.typography.h3)
-           Text(text = "#${int}",style = MaterialTheme.typography.h3)
+           Text(text = "You have $int saved news items",style = MaterialTheme.typography.h3)
         }
     }
 }
