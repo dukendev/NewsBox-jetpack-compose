0db69aaaa4fb32345634840eb216af2b6dadb074
Sanjeet Yadav <ysanjeet535@gmail.com>
2021-10-24 15:49:30 +0530

    splash screen working

--- app/src/main/java/com/ysanjeet535/newsbox/MainActivity.kt
+++ app/src/main/java/com/ysanjeet535/newsbox/MainActivity.kt
@@ -8,12 +8,16 @@ import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.*
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.rememberNavController
 import com.ysanjeet535.newsbox.ui.custom_components.BottomNavBar
 import com.ysanjeet535.newsbox.ui.navigation.NavigationComponent
 import com.ysanjeet535.newsbox.ui.navigation.Screens
 import com.ysanjeet535.newsbox.ui.theme.NewsBoxTheme
+import com.ysanjeet535.newsbox.ui.view.home.SplashScreen
 import com.ysanjeet535.newsbox.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,23 +39,28 @@ class MainActivity : ComponentActivity() {
             NewsBoxTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    
-                    Scaffold(
-                        bottomBar = {
+                    var showSplashScreen by remember { mutableStateOf(true)}
+                    val onTimeout : ()->Unit = { showSplashScreen = false}
 
-                            BottomNavBar(currentScreen = currentScreen.value.name,navController){
-                                currentScreen.value = it
+                    if(showSplashScreen){
+                        SplashScreen(onTimeout = onTimeout)
+                    }else{
+                        Scaffold(
+                            bottomBar = {
+
+                                BottomNavBar(currentScreen = currentScreen.value.name,navController){
+                                    currentScreen.value = it
+                                }
                             }
-                        }
-                    ) {
+                        ) {
                             NavigationComponent(
                                 navController = navController,
                                 paddingValues = it.calculateBottomPadding(),
                                 mainViewModel = mainViewModel
                             )
-                        
-                    }
 
+                        }
+                    }
 
                 }
             }
